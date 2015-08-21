2bd707437fa5fb93e61ecfd16361446d3e4f58cd
ethan-nelson <emann15@hotmail.com>
2015-08-20 22:05:09 -0500

    Corrected exception handling for userUtil request

--- osmhm/userUtil.py
+++ osmhm/userUtil.py
@@ -6,7 +6,7 @@ import xml.etree.cElementTree as ElementTree
 def userUtil(user_id):
     def parseUser(source, handle):
         for event, elem in ElementTree.iterparse(source,
-                                                 events=('start', 'end')):
+                                                 events=('start')):
             if event == 'start':
                 handle.startElement(elem.tag, elem.attrib)
             elem.clear()
@@ -31,10 +31,10 @@ def userUtil(user_id):
         url = 'http://www.osm.org/api/0.6/user/' + str(user_id)
 
         content = requests.get(url)
-        content = StringIO.StringIO(content.content)
+        if content.status_code == 404:
+            raise Exception
 
-        if content.len == 0:
-            raise Exception #Yeah, I raised a general exception and I don't really care.
+        content = StringIO.StringIO(content.content)
 
         dataObject = userDecoder()
         parseUser(content, dataObject)
