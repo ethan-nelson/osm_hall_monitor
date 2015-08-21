75d2c7be6629f40efb802590e190a42731205eaa
ethan-nelson <emann15@hotmail.com>
2015-08-20 22:44:45 -0500

    flake8 fixes for connect.py

--- osmhm/connect.py
+++ osmhm/connect.py
@@ -9,13 +9,13 @@ def connect():
     dburl = urlparse.urlparse(os.environ['DATABASE_URL'])
     try:
         conn = psycopg2.connect(
-                 database=dburl.path[1:],
-                 user=dburl.username,
-                 password=dburl.password,
-                 host=dburl.hostname,
-                 port=dburl.port,
-                 cursor_factory=DictCursor,
-                 )
+            database=dburl.path[1:],
+            user=dburl.username,
+            password=dburl.password,
+            host=dburl.hostname,
+            port=dburl.port,
+            cursor_factory=DictCursor,
+            )
     except:
         print 'Error connecting to database!'
         return None
