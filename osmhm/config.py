c5a4b3491c541da4cf9dd7625c2c5686d6b9cf5f
ethan-nelson <emann15@hotmail.com>
2015-09-23 22:05:17 -0500

    Added try/except blocks and warnings for config

--- osmhm/config.py
+++ osmhm/config.py
@@ -1,7 +1,21 @@
 import os
 import urlparse
+import warnings
 
-database_url = os.environ['DATABASE_URL']
-email_user = os.environ['EMAIL_USER']
-email_password = os.environ['EMAIL_PASS']
-email_server = os.environ['EMAIL_SERVER']
+
+try:
+    database_url = os.environ['DATABASE_URL']
+except:
+    warnings.warn('No database_url detected; please set it with config.', UserWarning)
+try:
+    email_user = os.environ['EMAIL_USER']
+except:
+    warnings.warn('No email_user detected; please set it with config.', UserWarning)
+try:
+    email_password = os.environ['EMAIL_PASS']
+except:
+    warnings.warn('No email_password detected; please set it with config.', UserWarning)
+try:
+    email_server = os.environ['EMAIL_SERVER']
+except:
+    warnings.warn('No email_server detected; please set it with config.', UserWarning)
