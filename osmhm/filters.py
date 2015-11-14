d49ae38903656e61012fc7c2a60a121a485ac547
ethan-nelson <emann15@hotmail.com>
2015-11-13 21:01:03 -0600

    User filter wildcard match

--- osmhm/filters.py
+++ osmhm/filters.py
@@ -79,7 +79,7 @@ def userFilter(changesets, notification=False):
     if watched_users:
         for changesetid, changeset in changesets.iteritems():
             for user in watched_users:
-                if changeset['username'] == user['username']:
+                if fnmatch.fnmatch(changeset['username'],user['username']):
                     info = (changeset['timestamp'], changesetid,
                             changeset['username'].encode('utf8'),
                             changeset['create'], changeset['modify'],
