acc870ab49262faafe926c5891a976e11225eef1
Ethan Nelson <git@ethan-nelson.com>
2016-09-05 22:08:15 -0500

    Push to new version

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
 setup(name='osm_hall_monitor',
-      version='0.42',
+      version='0.43',
       description='Passive changeset monitoring for OpenStreetMap.',
       url='http://github.com/ethan-nelson/osm_hall_monitor',
       author='Ethan Nelson',
       license='MIT',
-      author_email='ethan-nelson@users.noreply.github.com',
+      author_email='git@ethan-nelson.com',
       install_requires = ['psycopg2','osm_diff_tool'],
-      packages=['osmhm'],
-      zip_safe=False)
+      packages=['osmhm'])
       
