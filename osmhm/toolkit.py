3265225f84c5574bf88eb6fbf6a3df2727a49d58
Ethan Nelson <git@ethan-nelson.com>
2016-09-16 16:16:13 -0500

    Remove deprecated tools

--- osmhm/toolkit.py
+++
@@ -1,7 +0,0 @@
-import math
-
-
-def distance_between_nodes(node1, node2):
-    dlat = math.fabs(node1['lat'] - node2['lat'])
-    dlon = math.fabs(node1['lon'] - node2['lon'])
-    return math.hypot(dlat, dlon)
