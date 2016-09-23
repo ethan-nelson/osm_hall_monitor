1fc2465ce9bb438d09c381ecebc9a2627ce066dc
Ethan Nelson <git@ethan-nelson.com>
2016-09-23 04:39:45 -0500

    Bump version to match site

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='osm_hall_monitor',
-      version='0.50',
+      version='0.52',
       description='Passive changeset monitoring for OpenStreetMap.',
       url='http://github.com/ethan-nelson/osm_hall_monitor',
       author='Ethan Nelson',
