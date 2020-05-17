8e9d5ad00f3e094938b1704c368f254faf501401
Ethan Nelson <git@ethan-nelson.com>
2020-05-17 16:59:03 -0500

    Add more tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -9,3 +9,58 @@ def test_add_watched_user():
     results = cur.fetchall()
 
     assert len(results) == 1
+
+
+def test_remove_watched_user():
+    manage.remove_watched_user('testuser')
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM watched_users;")
+    results = cur.fetchall()
+
+    assert len(results) == 0
+
+
+def test_add_watched_object():
+    manage.add_watched_object('n322', 'Because', 'testadmin', 2, 'test@test.com')
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM watched_objects;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+
+def test_remove_watched_object():
+    manage.remove_watched_object('n322')
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM watched_objects;")
+    results = cur.fetchall()
+
+    assert len(results) == 0
+
+
+def test_add_watched_key():
+    manage.add_watched_key('railway', 'abandoned', 'Because', 'testadmin', 2, 'test@test.com')
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM watched_keys;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+
+def test_remove_watched_key():
+    manage.remove_watched_key('railway', 'abandoned')
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM watched_keys;")
+    results = cur.fetchall()
+
+    assert len(results) == 0
