680443e17bd349587d861851602b42e75573e92f
Ethan Nelson <git@ethan-nelson.com>
2020-05-17 19:11:12 -0500

    Add additional tests for pruning

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,4 @@
-from osmhm import connect, manage
+from osmhm import connect, inserts, manage
 
 def test_add_watched_user():
     manage.add_watched_user('testuser', 'Because', 'testadmin', 2, 'test@test.com')
@@ -61,6 +61,105 @@ def test_remove_watched_key():
     conn = connect.connect()
     cur = conn.cursor()
     cur.execute("SELECT * FROM watched_keys;")
+    results = cur.fetchall()
+
+    assert len(results) == 0
+
+
+def test_insert_user_event():
+    inserts.insert_user_event({'id': 1,
+                              'timestamp': '2018-01-01T00:00:00Z',
+                              'uid': 2,
+                              'create': 1,
+                              'modify': 2,
+                              'delete': 0}, 1)
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM history_users;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+
+def test_prune_user_event():
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM history_users;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+    cur.execute("""DELETE FROM history_users WHERE TO_TIMESTAMP(timestamp, 'YYYY-MM-DDTHH:MI:SSZ') < NOW() - INTERVAL '30 days';""")
+    conn.commit()
+
+    cur.execute("SELECT * FROM history_users;")
+    results = cur.fetchall()
+
+    assert len(results) == 0
+
+
+def test_insert_object_event():
+    inserts.insert_object_event({'timestamp': '2018-01-01T00:00:00Z',
+                                 'username': 'testuser',
+                                 'uid': 2,
+                                 'action': 1,
+                                 'changeset': 20}, 10)
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM history_objects;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+
+def test_prune_object_event():
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM history_objects;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+    cur.execute("""DELETE FROM history_objects WHERE TO_TIMESTAMP(timestamp, 'YYYY-MM-DDTHH:MI:SSZ') < NOW() - INTERVAL '30 days';""")
+    conn.commit()
+
+    cur.execute("SELECT * FROM history_objects;")
+    results = cur.fetchall()
+
+    assert len(results) == 0
+
+
+def test_insert_key_event():
+    inserts.insert_key_event({'id': 'n20',
+                              'timestamp': '2018-01-01T00:00:00Z',
+                              'username': 'testuser',
+                              'uid': 2,
+                              'action': 2,
+                              'tags': {'railway': 'abandoned'},
+                              'changeset': 21}, 'railway', 12)
+
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM history_keys;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+
+def test_prune_key_event():
+    conn = connect.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM history_keys;")
+    results = cur.fetchall()
+
+    assert len(results) == 1
+
+    cur.execute("""DELETE FROM history_keys WHERE TO_TIMESTAMP(timestamp, 'YYYY-MM-DDTHH:MI:SSZ') < NOW() - INTERVAL '30 days';""")
+    conn.commit()
+
+    cur.execute("SELECT * FROM history_keys;")
     results = cur.fetchall()
 
     assert len(results) == 0
